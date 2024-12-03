8ee1ecc48d68133015b00b676b89d61506998467
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-12-03 14:52:02 +0100

    Removed a lot of unnecessary classes streamlined the app to more or less resemle an ecommerce app

--- src/main/java/com/semicolon/africa/Go_Rent_App/repository/RentalTransactionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.semicolon.africa.Go_Rent_App.repository;
-
-import com.semicolon.africa.Go_Rent_App.models.RentalTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalTransactionRepo extends JpaRepository<RentalTransaction, Long> {
-}
--- src/test/java/com/semicolon/africa/Go_Rent_App/services/RenterTransactionServicesTest.java
+++ src/test/java/com/semicolon/africa/Go_Rent_App/services/RenterTransactionServicesTest.java
@@ -2,12 +2,17 @@ package com.semicolon.africa.Go_Rent_App.services;
 
 import com.semicolon.africa.Go_Rent_App.constants.Category;
 import com.semicolon.africa.Go_Rent_App.dtos.request.EventProductRequest;
+import com.semicolon.africa.Go_Rent_App.dtos.request.UserRequest;
 import com.semicolon.africa.Go_Rent_App.dtos.response.EventProductResponse;
+import com.semicolon.africa.Go_Rent_App.models.EventProduct;
+import com.semicolon.africa.Go_Rent_App.models.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+
+import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
 public class RentalTransactionServicesTest {
@@ -18,15 +23,18 @@ public class RentalTransactionServicesTest {
     @Test
     public void testThatUserCanAddRental(){
         EventProductRequest request = buildRequest();
-        EventProductResponse response = eventProductService.createEventProduct(request);
+        EventProduct response = eventProductService.createEventProduct(request);
+        assertNotNull(response);
    }
    private EventProductRequest buildRequest(){
         EventProductRequest product = new EventProductRequest();
+        product.setUserEmail("Kimdhaviex@gmail.com");
         product.setProductName("Photographic Camera");
         product.setProductCategory(Category.MEDIA);
         product.setPrice(BigDecimal.valueOf(10000));
         product.setDescription("I would like to get this camera");
         return product;
     }
+
 
 }
